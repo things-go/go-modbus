43f9232d795d14a541bbbd2db00e1dbbe6229817
mo <thinkgo@aliyun.com>
2019-10-21 15:06:55 +0800

    fix ci fix revive

--- cover.sh
+++
@@ -1,5 +0,0 @@
-mkdir -p coverHtml
-go test -coverpkg=./... -coverprofile=coverHtml/coverage.data ./...
-go tool cover -html=coverHtml/coverage.data -o coverHtml/coverage.html
-go tool cover -func=coverHtml/coverage.data -o coverHtml/coverage.txt
-/bin/bash -c 'firefox $PWD/coverHtml/coverage.html'
--- revive.sh
+++ revive.sh
@@ -1,2 +1,3 @@
+#!/usr/bin/env bash
 revive -config revive.toml  -formatter friendly ./...
 golangci-lint run
